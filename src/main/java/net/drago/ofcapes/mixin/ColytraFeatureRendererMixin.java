e918d4b773ae55c7bc959ed0e3c2140b970ba65a
MrMelon <github@onpointcoding.net>
2021-07-03 16:47:23 +0100

    Make sure to extend the elytra feature renderer

--- src/main/java/net/drago/ofcapes/mixin/ColytraFeatureRendererMixin.java
+++ src/main/java/net/drago/ofcapes/mixin/ColytraFeatureRendererMixin.java
@@ -6,6 +6,10 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerModelPart;
+import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLoader;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
@@ -19,10 +23,14 @@ import top.theillusivec4.colytra.client.ColytraFeatureRenderer;
 
 @Pseudo
 @Environment(value = EnvType.CLIENT)
-@Mixin(value = ColytraFeatureRenderer.class, remap = false)
-public class ColytraFeatureRendererMixin<T extends LivingEntity> {
+@Mixin(value = ColytraFeatureRenderer.class)
+public class ColytraFeatureRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends ElytraFeatureRenderer<T, M> {
     private static final Identifier ELYTRA_SKIN = new Identifier("textures/entity/elytra.png");
     private T livingEntity;
+
+    public ColytraFeatureRendererMixin(FeatureRendererContext<T, M> context, EntityModelLoader loader) {
+        super(context, loader);
+    }
 
     @Inject(method = "render", at = @At("HEAD"))
     public void captureLivingEntity(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
